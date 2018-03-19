4820a3cf1069bf95c5158e5fedf64f051cc43efe
TestAccountsUser <checkcheck19@gmail.com>
2018-03-19 23:46:57 +0100

    ajout configurations

--- clientui/src/main/java/com/clientui/ClientUiApplication.java
+++ clientui/src/main/java/com/clientui/ClientUiApplication.java
@@ -2,10 +2,12 @@ package com.clientui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.clientui")
+@EnableDiscoveryClient
 public class ClientUiApplication {
 
 	public static void main(String[] args) {
