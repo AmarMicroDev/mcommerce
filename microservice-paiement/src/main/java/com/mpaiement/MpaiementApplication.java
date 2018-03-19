3e38091c32d7f341246740a862e7128b9e975376
TestAccountsUser <checkcheck19@gmail.com>
2018-03-19 23:25:34 +0100

    Ajout Eureka

--- microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
+++ microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
@@ -2,10 +2,12 @@ package com.mpaiement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.mpaiement")
+@EnableDiscoveryClient
 public class MpaiementApplication {
 
 	public static void main(String[] args) {
--- microservice-produits/src/main/java/com/mproduits/MproduitsApplication.java
+++ microservice-produits/src/main/java/com/mproduits/MproduitsApplication.java
@@ -4,9 +4,11 @@ import com.mproduits.configurations.ApplicationPropertiesConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableDiscoveryClient
 public class MproduitsApplication {
 
 	public static void main(String[] args) {
