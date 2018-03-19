0b1e8a79757e167b5c7c381745f6d5eeeaaf0e2d
TestAccountsUser <checkcheck19@gmail.com>
2018-03-19 23:56:24 +0100

    ajout configurations

--- clientui/src/main/java/com/clientui/ClientUiApplication.java
+++ clientui/src/main/java/com/clientui/ClientUiApplication.java
@@ -3,6 +3,7 @@ package com.clientui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
--- clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
@@ -1,6 +1,7 @@
 package com.clientui.proxies;
 
 import com.clientui.beans.ProductBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 import java.util.Optional;
 
-@FeignClient(name = "microservice-produits", url = "localhost:9001")
+@FeignClient(name = "microservice-produits")
+@RibbonClient(name = "microservice-produits")
 public interface MicroserviceProduitsProxy {
 
     @GetMapping(value = "/Produits")
