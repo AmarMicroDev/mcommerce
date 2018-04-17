9b46bd57d82d965a321ec88716b7fac59a3061bd
TestAccountsUser <checkcheck19@gmail.com>
2018-04-17 13:43:54 +0200

    Client fonctionnel communiquant avec tous les MS

--- clientui/src/main/java/com/clientui/ClientUiApplication.java
+++ clientui/src/main/java/com/clientui/ClientUiApplication.java
@@ -1,8 +1,13 @@
 package com.clientui;
 
+import com.clientui.exceptions.CustomErrorDecoder;
+import com.clientui.proxies.MicroserviceProduitsProxy;
+import feign.Feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.support.SpringMvcContract;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients("com.clientui")
@@ -10,5 +15,13 @@ public class ClientUiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientUiApplication.class, args);
+
+	/*	Feign.builder()
+				.contract(new SpringMvcContract())
+				.errorDecoder(new CustomErrorDecoder())
+				.target(MicroserviceProduitsProxy.class, "localhost:9001");
+				*/
 	}
+
+
 }
